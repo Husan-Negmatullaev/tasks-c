bef8819c55aa74697f25faa9aeebc63a03aeebdf
Husan-Negmatullaev <husannegmatullaev21@gmail.com>
2023-07-12 21:35:37 +0500

    chapter-2

---
+++ chapter-2/task-4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) /* программа выводит количество недель в году */
+{
+    int count_week;
+
+    count_week = 56;
+
+    printf("B году %d недель.", count_week);
+
+    return 0;
+}
